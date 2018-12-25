46a95e0d3270c5cdce09ca7761a23c68a94c6b32
Jonathan Chew <jchew513@gmail.com>
2018-12-24 22:32:54 -0800

    initial commit

---
+++ app/src/main/java/com/example/jonathan/bluetoothtest/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.jonathan.bluetoothtest;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private class ConnectThread extends Thread {
+
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device){
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+            try{
+                tmp = device.createRfcommSocketToServiceRecord(UUID.randomUUID());
+            }catch(IOException e){
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        @Override
+        public void run(){
+            mBluetoothAdapter.cancelDiscovery();
+            try{
+                mmSocket.connect();
+            }catch(IOException connectException){
+                try{
+                    mmSocket.close();
+                }catch(IOException closeException){
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+        }
+
+        public void cancel(){
+            try{
+                mmSocket.close();
+            }catch(IOException e){
+                Log.e(TAG, "Could not close client socket", e);
+            }
+        }
+    }
+
+    private static final String TAG = "BluetoothTest";
+
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    String selectedDeviceName;
+    String selectedDeviceAddress;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress();
+            }
+        }
+    };
+
+    public void discoverBtDevices(View view){
+        mBluetoothAdapter.startDiscovery();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+        mBluetoothAdapter.cancelDiscovery();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+
+    public void enableBluetooth(View view){
+        if(mBluetoothAdapter == null){
+            Log.d(TAG, "Device doesn't support Bluetooth");
+        } else {
+            if (mBluetoothAdapter.isEnabled()) {
+                Log.d(TAG, "Bluetooth is already enabled");
+            } else {
+                Log.d(TAG, "Bluetooth is not enabled");
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    public void getPairedDevices(View view){
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0){
+            Log.d(TAG, "Paired Devices:");
+            for(BluetoothDevice device : pairedDevices){
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress();
+                Log.d(TAG, "name: " + deviceName + " address: " + deviceHardwareAddress);
+            }
+        }
+    }
+
+    public void connectToDevice(View view) {
+//        ConnectThread connection = new ConnectThread();
+//        connection.Start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActviityResult: " + Integer.toString(requestCode));
+    }
+}
